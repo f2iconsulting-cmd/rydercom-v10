3a007710169a0f66b05be337183b3e4b8c8a5873
f2iconsulting-cmd <f2i.consulting@gmail.com>
2026-06-06 11:30:10 +0000

    fix: import flow.collect manquant

--- android/app/src/main/java/com/rydercom/nativeaudio/RyderComForegroundService.kt
+++ android/app/src/main/java/com/rydercom/nativeaudio/RyderComForegroundService.kt
@@ -18,6 +18,7 @@ import io.livekit.android.room.Room
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class RyderComForegroundService : Service() {
